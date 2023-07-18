1b78cf9f0e20a2b50a5140415ea242eaa8da6b3b
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-07-18 14:57:21 -0300

    ok

--- livraria/src/App.js
+++ livraria/src/App.js
@@ -6,9 +6,10 @@ const App = () => {
 
   return (
     <div className='App'>
+    <div className='logo'>
+      <div className='logo__content'><h1>LIVRARIA</h1></div>
+    </div>
     <div className='container'>
-      <h1>Livraria</h1>
-
       <LivrosList  />
     </div>
     </div>
--- livraria/src/LivrosList.js
+++ livraria/src/LivrosList.js
@@ -35,24 +35,32 @@ const TodoList = () => {
 
 
   return (
-    <div>
+    <div className='form__list'>
       <div className='form'>
+      <h2>Cadastrar livro</h2>
       <LivroForm onNewBook={handleNewBook} existingNames={books} />
       </div>
       
-
-      <h1>Livros</h1>
+      <div className='books__background'>
+      <h2>Livros cadastrados</h2>
       <div className='books__container'>
         {books.map((book) => (
-          <div key={book.titulo}>
-            <p>Nome: {book.titulo}</p>
+          <div key={book.titulo} className='book'>
+          <div className='book__icons'>
+          <h3>{book.titulo}</h3>
+          <div>
+          <button onClick={() => handleDeleteBook(book.titulo)}><i class="uil uil-edit"></i></button>
+          <button onClick={() => handleDeleteBook(book.titulo)}><i class="uil uil-times"></i></button>
+          </div>
+          </div>
+            
             <p>Autor: {book.autor}</p>
             <p>Editora: {book.editora}</p>
             <p>Ano de publicação: {book.anoPublicacao}</p>
             <p>Número de páginas: {book.numeroPaginas}</p>
-            <button onClick={() => handleDeleteBook(book.titulo)}>Apagar</button>
             </div>
         ))}
+      </div>
       </div>
       </div>
   );
